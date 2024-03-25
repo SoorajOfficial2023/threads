7f76fe2d47121eb7c55b1356a3184c87a1210f14
sooraj <soorajkknmd@gmail.com>
2024-03-25 14:47:12 +0530

    multithread_for_loop

--- thread.py
+++ thread.py
@@ -7,15 +7,12 @@ def do_something():
     print('sleeping 1 second')
     time.sleep(1)
     print('done sleeping')
-
-t1 = threading.Thread(target = do_something)
-t2 = threading.Thread(target = do_something)
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-
-
-
+threads = []
+for _ in range(10):
+    t = threading.Thread(target = do_something)
+    t.start()
+    threads.append(t)
+for thread in threads:
+    thread.join()
 finish = time.perf_counter()
 print(f"finished in {round(finish-start,2)} seconds")
